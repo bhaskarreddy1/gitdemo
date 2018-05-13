4b6fa26242d7664d7e88462d6489b2ae6dc64794
MANASVI <chelurubhaskarreddy@gmail.com>
2018-05-13 19:54:04 +0530

    Add bo class

---
+++ demoproject/src/demoproject/BoClass.java
@@ -0,0 +1,36 @@
+package demoproject;
+
+public class BoClass {
+	private	String name;
+	
+	private	String course;
+	private	String gender;
+	private	String city;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+
+}
